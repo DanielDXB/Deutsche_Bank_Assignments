e6dce52aa889953a91451b499f9ba15afb16a19a
Daniel Bolontoc <danielbolontoc.2001@gmail.com>
2022-05-13 23:43:21 +0300

    Add prev content

---
+++ JSHW/js/script.js
@@ -0,0 +1,16 @@
+const modal_open = document.getElementById("open");
+const modal_cancel = document.getElementById("cancel");
+const modal_container = document.getElementById("modal_container");
+const addRandomArticle = document.getElementById("randomArticleGenerator");
+
+modal_open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+modal_cancel.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
+
+addRandomArticle.addEventListener("click", () => {
+    console.log("it works");
+});
