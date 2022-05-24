ab310f6aa307e27a40535d0cdf2d6e853cfeaf0a
Daniel Bolontoc <danielbolontoc.2001@gmail.com>
2022-05-24 14:09:08 +0300

    final commit

--- ANGULARHW/blog-app/src/app/app.component.spec.ts
+++ ANGULARHW/blog-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog-app'`, () => {
+  it(`should have as title 'travelling-blog-v4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog-app');
+    expect(app.title).toEqual('travelling-blog-v4');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('blog-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('travelling-blog-v4 app is running!');
   });
 });
--- ANGULARHW/blog-app/src/app/app.module.ts
+++ ANGULARHW/blog-app/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticleComponent } from './components/article/article.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddComponent } from './components/add/add.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    MenuComponent,
+    ArticleComponent,
+    FooterComponent,
+    AddComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
